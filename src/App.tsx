73995f32bea03f8a84dbc77145f7bb895949fd38
Mathieu T. <tang.mathieu@gmail.com>
2025-03-08 16:34:08 +0100

    fix html body css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <h1 className="mx-auto">coucou</h1>;
+  return (
+    <div>
+      <h1 className="ml-auto mr-auto w-max text-3xl font-extrabold">
+        Quizzstand
+      </h1>
+    </div>
+  );
 }
 
 export default App;
